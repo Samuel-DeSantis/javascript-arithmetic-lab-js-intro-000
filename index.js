19d32717ba4ed2b5322540db87bf5d28bd466e25
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-07-10 05:31:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function add(x, y) {return x + y;}
 function subtract(x, y) {return x - y;}
 function multiply(x, y) {return x * y;}
 function divide(x, y) {return x / y;}
-function increment(n) {return n++;}
+function increment(n) {return ++n;}
 function makeInt(n) {}
 function preserveDecimal(n) {return parseInt(n, 10);}
 function preserveDecimal(n) {return parseFloat(n);}
